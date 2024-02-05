a2aaf2e555714922132bd121f183010bbf04e9c7
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2024-02-05 12:42:33 -0600

    Add Q&A for MP2.

---
+++ examples/mp2/Interact.java
@@ -0,0 +1,26 @@
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * A simple example of interaction.
+ */
+public class Interact {
+  public static void main(String[] args) {
+    PrintWriter pen = new PrintWriter(System.out, true);
+    Scanner eyes = new Scanner(System.in);
+    pen.print("Enter an integer: ");
+    pen.flush();
+    String stuff = eyes.nextLine();
+    BigInteger i = new BigInteger(stuff);
+    pen.printf("square(%s) = %s\n", i.toString(), i.multiply(i).toString());
+    pen.print("Enter a bunch of integers separated by spaces: ");
+    pen.flush();
+    stuff = eyes.nextLine();
+    String[] nums = stuff.split(" ");
+    for (String num : nums) {
+      i = new BigInteger(num);
+      pen.printf("square(%s) = %s\n", i.toString(), i.multiply(i).toString());
+    } // for
+  } // main(String[])
+} // class Interact
